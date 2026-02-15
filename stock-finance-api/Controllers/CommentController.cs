6fad86da8d3323473a9073de77cf26dbc3179618
tijes <tijes@DESKTOP-D6TFKBH>
2026-02-15 08:10:34 +0100

    Add Data Validation

--- stock-finance-api/Controllers/CommentController.cs
+++ stock-finance-api/Controllers/CommentController.cs
@@ -25,6 +25,9 @@ namespace stock_finance_api.Controllers
 		[HttpGet]
 		public async Task<IActionResult> GetAllAsync()
 		{
+			if (!ModelState.IsValid) 
+				return BadRequest(ModelState);
+
 			var comments = await _commentRepo.GetAllAsync();
 
 			var commentDto = comments.Select(c => c.ToCommentDto());
@@ -32,9 +35,12 @@ namespace stock_finance_api.Controllers
 			return Ok(commentDto);
 		}
 
-		[HttpGet("{id}")]
+		[HttpGet("{id:int}")]
 		public async Task<IActionResult> GetById([FromRoute] int id)
 		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
 			var comment = await _commentRepo.GetByIdAsync(id);
 
 			if (comment == null)
@@ -48,6 +54,9 @@ namespace stock_finance_api.Controllers
 		[HttpPost("{stockId}")]
 		public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
 		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
 			if (!await _stockRepo.StockExists(stockId))
 			{
 				return BadRequest($"Stock does not exist.");
@@ -60,9 +69,12 @@ namespace stock_finance_api.Controllers
 		}
 
 		[HttpPut]
-		[Route("{id}")]
+		[Route("{id:int}")]
 		public async Task<IActionResult> Update([FromRoute] int id, UpdateCommentRequestDto updateDto)
 		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
 			var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentfromUpdate());
 
 			if (comment == null)
@@ -74,9 +86,12 @@ namespace stock_finance_api.Controllers
 		}
 
 		[HttpDelete]
-		[Route("{id}")]
+		[Route("{id:int}")]
 		public async Task<IActionResult> Delete([FromRoute] int id)
 		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
 			var commentModel = await _commentRepo.DeleteAsync(id);
 
 			if (commentModel == null)
