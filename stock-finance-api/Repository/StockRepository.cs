2bcbeb414c3db6de75e26ecb984a391acdf50751
tijes <tijes@DESKTOP-D6TFKBH>
2026-02-15 15:55:25 +0100

    Add Pagination

--- stock-finance-api/Repository/StockRepository.cs
+++ stock-finance-api/Repository/StockRepository.cs
@@ -59,7 +59,9 @@ namespace stock_finance_api.Repository
 				};
 			}
 
-			return await stocks.ToListAsync();
+			var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+			return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
 		}
 
 		public async Task<Stock?> GetByIdAsync(int id)
