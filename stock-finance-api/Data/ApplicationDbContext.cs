9ef700d5a1e4f34f1ba840d1d06185f6dd28c1da
tijes <tijes@DESKTOP-D6TFKBH>
2026-02-16 05:01:12 +0100

    Add Register

--- stock-finance-api/Data/ApplicationDbContext.cs
+++ stock-finance-api/Data/ApplicationDbContext.cs
@@ -6,9 +6,10 @@ using stock_finance_api.Models;
 
 namespace stock_finance_api.Data
 {
-		public class ApplicationDbContext : IdentityDbContext<AppUser> {
+	public class ApplicationDbContext : IdentityDbContext<AppUser>
+	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-		:base(options)
+		: base(options)
 		{
 
 		}
@@ -16,5 +17,21 @@ namespace stock_finance_api.Data
 		public DbSet<Stock> Stocks { get; set; }
 
 		public DbSet<Comment> Comments { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			List<IdentityRole> roles = new List<IdentityRole>
+			{
+				new IdentityRole {
+					Name = "Admin",
+					NormalizedName = "ADMIN" },
+				new IdentityRole {
+					Name = "User",
+					NormalizedName = "USER" }
+			};
+			builder.Entity<IdentityRole>().HasData(roles);
+		}
 	}
 }
