4ab2cfbc7752c8c6f7ef222e293089ca5dfa9409
tijes <tijes@DESKTOP-D6TFKBH>
2026-02-21 04:50:59 +0100

    Add Portfolio CREATE

--- stock-finance-api/Interfaces/IPortfolioRepository.cs
+++ stock-finance-api/Interfaces/IPortfolioRepository.cs
@@ -5,5 +5,7 @@ namespace stock_finance_api.Interfaces
 	public interface IPortfolioRepository
 	{
 		Task<List<Stock>> GetUserPortfolio(AppUser user);
+
+		Task<Portfolio> CreateAsync(Portfolio portfolio);
 	}
 }
--- stock-finance-api/Repository/StockRepository.cs
+++ stock-finance-api/Repository/StockRepository.cs
@@ -69,6 +69,11 @@ namespace stock_finance_api.Repository
 			return await _context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(x => x.Id == id);	
 		}
 
+		public async Task<Stock?> GetBySymbolAsync(string symbol)
+		{
+			return await _context.Stocks.FirstOrDefaultAsync(s => s.Symbol == symbol);
+		}
+
 		public Task<bool> StockExists(int id)
 		{
 			return _context.Stocks.AnyAsync(x => x.Id == id);
